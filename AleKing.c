4fbe6e49042db5d7a169ef897549c969a9ad620f
erisso3 <42158816+erisso3@users.noreply.github.com>
2018-09-02 23:30:14 -0500

    Add files via upload

---
+++ AleKing.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int a,*b,op,i,j,aux,m;
+	scanf("%d",&a);
+	b=(int*)malloc(a*sizeof(int));
+	for(int i=0;i<a;i++){
+		scanf("%d",&b[i]);
+	}
+	scanf("%d",&op);
+	switch(op){
+	case 1:
+		for(int i=a-1;i>=0;i--){
+			printf("%d ",b[i]);
+		}
+		break;
+	case 2:
+		for(j=0;j<a;j++){
+			printf("%d ",b[j]);
+		}
+		break;
+	case 3:
+		for(i=0;i<a;i++){
+			m=i;
+			for(j=i+1;j<a;j++){
+				if(b[j]<b[m]){
+					m=j;
+				}
+			}
+			aux=b[i];
+			b[i]=b[m];
+			b[m]=aux;
+		}
+		for(i=a-1;i>=0;i--){
+			printf("%d ",b[i]);
+		}
+		break;
+	default:
+		break;
+	}
+	
+	return 0;
+}
+
---
+++ Canguros.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+	int C,N;
+	int	v[1000000],cn=0,t=0;
+	char l;
+	scanf("%d" "%d",&C,&N);
+	while(N!=0){
+		scanf("%c",&l);
+		switch(l){
+		case 'A':
+			C=C-v[0];
+			for(int i=0;i<(t-1);i++){
+				v[i]=v[i+1];
+			}
+			t--;
+			cn--;
+			N--;
+			break;
+		case 'C':
+			printf("%i\n",t);
+			N--;
+			break;
+
+		case 'N':
+			scanf("%d",&v[cn]);
+			v[cn]++;
+			cn++;
+			t++;
+			N--;
+			break;
+		case 'R':
+			printf("%i\n",C);	
+			N--;
+			break;
+		}       
+	}
+	return 0;
+}
